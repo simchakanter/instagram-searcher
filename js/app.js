c8ff5e64842a334546ac887827a935b17fdd4400
Simcha Kanter <simchakanter@gmail.com>
2014-07-31 15:46:24 -0500

    Initial commit (everything works)

---
+++ js/app.js
@@ -0,0 +1,31 @@
+var instaSearch = angular.module("instagramSearcherApp", []);
+
+instaSearch.controller("InstagramSearchCtrl", function($scope, $http) {
+  $scope.search = function(keyword) {
+    if (!keyword) {
+      $scope.formError = true;
+      return false;
+    }
+    $scope.keyword = keyword;
+    $scope.formError = false;
+    var url = "https://api.instagram.com/v1/tags/" + keyword + "/media/recent";
+    var request = {
+      callback:"JSON_CALLBACK",
+      client_id: "4940fe38623144c6b8cc6ee7c6a8c7a2"
+    };
+    $http({
+      method: "JSONP",
+      url: url,
+      params: request
+    })
+    .success(function(result) {
+      console.log(result);
+      $scope.results = result.data;
+      $scope.resultMessage = "We found " + $scope.results.length + " results for " + $scope.keyword;
+      $scope.keyword = "";
+    })
+    .error(function() {
+      alert('Error!');
+    });
+  };
+});
